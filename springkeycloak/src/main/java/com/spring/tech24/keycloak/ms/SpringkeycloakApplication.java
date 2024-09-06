66e85cc2e628aaa278e14cfcf887c9e21b68f31a
Mrinal Jana <jana.mrinalkanti@gmail.com>
2024-09-06 13:33:49 -0500

    Initial Commit

---
+++ springkeycloak/src/main/java/com/spring/tech24/keycloak/ms/SpringkeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tech24.keycloak.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkeycloakApplication.class, args);
+	}
+
+}
---
+++ springkeycloak/src/test/java/com/spring/tech24/keycloak/ms/SpringkeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tech24.keycloak.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
